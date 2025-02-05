7cca8b6647e60fc46d9b25448b0265d59479ee6a
LuisBBandeira <luisfilipebentobandeira@gmail.com>
2025-02-05 21:57:04 +0000

    aula_1

---
+++ aula_1/main.js
@@ -0,0 +1,17 @@
+const user = {
+    name: "",
+    email: ""
+}
+
+const medication = {
+    id: "",
+    name: "",
+    price: "",
+    dosage: ""
+}
+
+const order = {
+    id: "",
+    package: [],
+    timestamp: "" 
+}
