cdf3d248cdec1bd033a87a77e0e2f8f03d1eb406
LuisBBandeira <luisfilipebentobandeira@gmail.com>
2025-03-07 20:43:26 +0000

    aula_17

--- aula_9-23/next-project/src/components/ClientProviders.tsx
+++ aula_9-23/next-project/src/components/ClientProviders.tsx
@@ -1,8 +1,10 @@
 "use client";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "react-redux";
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from "./redux/store";
+
+const queryClient = new QueryClient();
 
 export default function ClientProviders({ 
   children 
@@ -12,7 +14,9 @@ export default function ClientProviders({
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        {children}
+          <QueryClientProvider client={queryClient}>
+            {children}
+          </QueryClientProvider>
       </PersistGate>
     </Provider>
   );
